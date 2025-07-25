05a18a859e3df0b8320c32e75f011861a509ac7f
nanonimous <cocnivaas@gmail.com>
2025-07-25 11:23:57 +0530

    final push before hostt 5

--- src/Layout/CopyRight/CopyRight.jsx
+++ src/Layout/CopyRight/CopyRight.jsx
@@ -3,22 +3,22 @@ import style from "./CopyRight.module.css";
 
 export function CopyRight() {
   const design = import.meta.env.VITE_DESIGN_LINK;
-
+  const year = new Date().getFullYear(); // Get current year
 
   return (
     <div className={style.container}>
       <h1 className={style.copy}>
-        <FaRegCopyright /> Copyright
+       Copyright © {year}
       </h1>
       <div className={style.created}>
-        Designed & Developed by{" "}
+        Designed & Developed by{"\u00A0"}
         <a
           href={design}
           target="_blank"
           rel="noopener noreferrer"
           className={style.link}
         >
-          Dept of IoT and Cybersecurity
+          Dept of CSE (IoT and Cybersecurity)
         </a>
       </div>
     </div>
